5ac5f52a20ccc7bccdf5c41fec3d4c777a4e28d0
Thamila92 <achatthamila@gmail.com>
2025-11-29 23:46:02 +0100

    update ngnix config

--- files/app_sources/index.php
+++ files/app_sources/index.php
@@ -6,15 +6,15 @@ $dbname = "webapp_db";
  
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Vérification de la connexion
+ 
 if ($conn->connect_error) {
-    echo "<h1>❌ DÉPLOIEMENT RÉUSSI, MAIS...</h1>";
+    echo "<h1> DÉPLOIEMENT RÉUSSI, MAIS...</h1>";
     echo "<p>La connexion à MySQL a échoué. Problème de réseau ou d'identifiants.</p>";
     die("Erreur: " . $conn->connect_error);
 }
 
  
-echo "<h1>✅ Bienvenue à KapsuleKorp!</h1>";
+echo "<h1> Bienvenue à KapsuleKorp!</h1>";
 echo "<h3>Infrastructure as Code (IaC) - Déploiement PHP-LEMP réussi!</h3>";
 echo "<p>La base de données <strong>'$dbname'</strong> a été jointe avec succès!</p>";
 
